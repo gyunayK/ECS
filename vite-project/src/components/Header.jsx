f7d0f081e352544c452c0cca36b4a89d058eedd9
OdinOfRivia <gyunay@hotmail.dk>
2023-03-29 21:54:27 -0700

    first commit

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header>
+    <div className="">
+      <div>
+        <h1>Cartier</h1>
+      </div>
+      <nav>
+        <ul>
+          <i className="fa-solid fa-bag-shopping"></i>
+          <i className="fa-solid fa-location-dot"></i>
+        </ul>
+        <ul>
+          <li>Home</li>
+          <li>Features</li>
+          <li>Products</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+    
+    </>
+  )
+}
+
+export default Header
